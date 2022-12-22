aefe8cd549b2ac7b565fdb07d8ae85457b53e6a6
alyahmady <better.aly.ahmady@gmail.com>
2022-12-22 12:49:48 +0330

    feat: add `BadDirection` and `BadSpaceGrid` exception classes

--- app/errors.py
+++ app/errors.py
@@ -14,9 +14,18 @@ class EmptySlot(CustomException):
     def __init__(self, message: str | None = "Slot is empty", code: int = 400):
         super().__init__(message, code)
 
+class BadSpaceGrid(CustomException):
+    def __init__(self, message: str | None = "Simulation space type is not valid", code: int = 400):
+        super().__init__(message, code)
+
 
 class WrongSlot(CustomException):
     def __init__(self, message: str | None = "Slot indexes are out of space ranges", code: int = 400):
+        super().__init__(message, code)
+
+
+class BadDirection(CustomException):
+    def __init__(self, message: str | None = "Direction to move is not valid", code: int = 500):
         super().__init__(message, code)
 
 
